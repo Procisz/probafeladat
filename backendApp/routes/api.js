6fa67f0a7d857ea0fc509e0a98f68d1f6d13be78
Procisz <agostonzajkas@gmail.com>
2020-03-04 15:12:05 +0100

    default sql added, /api works.

---
+++ backendApp/routes/api.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const DataBase = require("../modules/database");
+const dataBase = new DataBase();
+
+/* GET home page. */
+router.get("/", (req, res, next) => {
+  res.send("Api works.");
+});
+
+router.get("/angular", async (req, res, next) => {
+  let users = await dataBase.read();
+  res.json(users);
+});
+
+module.exports = router;
